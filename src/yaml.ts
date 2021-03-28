88449036c77d4dad0f5722358129f361bf8a786b
jj1uty <jj1uty@jarl.com>
2021-03-28 22:53:10 +0900

    クラスの削除

--- src/yaml.ts
+++ src/yaml.ts
@@ -13,57 +13,44 @@ type EncryptData = {[feald: string]: {
 
 type DecryptData = {[feald: string]: string}
 
-class CryptYaml {
-  private file: string;
-
-  private yamlString: string;
-
-  constructor(file: string) {
-    this.file = file;
-    this.yamlString = fs.readFileSync(this.file, {encoding: 'utf8'});
-  }
-
-  public encryptyYaml(password: string): string  {
-    const b: EncryptData = {};
-    this.yamlString.split(/\n|\r|\r\n/).map((line) => {
-      const v = line.match(/^\s*([\w.-]+)\s*=\s*(.*)?\s*$/)
-      if (v != null) {
-        const salt = crypto.randomBytes(32)
-        return {
-          key: v[1],
-          value: {
-            encrypt: encrypt(ALGO, password, salt, v[2] || ''),
-            algorithm: ALGO,
-            salt: encryptBase64(salt),
-          }
+const encryptyYaml = (yamlString: string, password: string): string => {
+  const b: EncryptData = {};
+  yamlString.split(/\n|\r|\r\n/).map((line) => {
+    const v = line.match(/^\s*([\w.-]+)\s*=\s*(.*)?\s*$/)
+    if (v != null) {
+      const salt = crypto.randomBytes(32)
+      return {
+        key: v[1],
+        value: {
+          encrypt: encrypt(ALGO, password, salt, v[2] || ''),
+          algorithm: ALGO,
+          salt: encryptBase64(salt),
         }
       }
-      return null;
-    }).forEach((v) => {
-      if(v !== null) {
-        b[v.key] = v.value;
-      }
-    })
-    return yaml.dump(b);
-  }
-  
-  public decryptyYaml(password: string): string {
-    const b = Object.entries(<EncryptData>yaml.load(this.yamlString)).map((v) => {
-      if (v[1] === undefined) {
-        throw new Error();
-      }
-      return {
-        key: v[0],
-        value: decrypt(v[1].algorithm ,password, decryptBase64(v[1].salt), v[1].encrypt),
-      }
-    }).map((v) => `${v.key}=${v.value}`);
-    return b.join('\n');
-  }
+    }
+    return null;
+  }).forEach((v) => {
+    if(v !== null) {
+      b[v.key] = v.value;
+    }
+  })
+  return yaml.dump(b);
 }
 
-const a = new CryptYaml('a');
-const e = a.encryptyYaml('abc');
+const decryptyYaml = (yamlString: string, password: string): string => {
+  const b = Object.entries(<EncryptData>yaml.load(yamlString)).map((v) => {
+    if (v[1] === undefined) {
+      throw new Error();
+    }
+    return {
+      key: v[0],
+      value: decrypt(v[1].algorithm ,password, decryptBase64(v[1].salt), v[1].encrypt),
+    }
+  }).map((v) => `${v.key}=${v.value}`);
+  return b.join('\n');
+}
+
+const e = encryptyYaml(fs.readFileSync('a', {encoding: 'utf8'}),'abc');
 fs.writeFileSync('b', e);
-const b = new CryptYaml('b');
-const d = b.decryptyYaml('abc');
+const d = decryptyYaml(fs.readFileSync('b', {encoding: 'utf8'}), 'abc');
 console.log(d);
