fbd1c396f2a8417537782051f0f7a2f9689d7cdd
jj1uty <jj1uty@jarl.com>
2021-08-17 08:54:08 +0900

    コメントと空行の処理と追加

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,10 @@ fn main() {
         csp_rng.fill_bytes(&mut data);
 
         let salt = &base64::encode(&data[..]);
+        if Regex::new(r"^(#.*|\s*)$").unwrap().is_match(&l) {
+            let _ = writeln!(file, "{}", l);
+            continue;
+        }
         let r = Regex::new(r"^([^=]+)=(.*)$").unwrap().captures(&l).unwrap();
         
         let b = crypto::encrypt(algo, passwd, salt, info, r.get(2).unwrap().as_str());
@@ -34,6 +38,10 @@ fn main() {
     let mut file2 = File::create("e").unwrap();
     for result in BufReader::new(File::open("d").unwrap()).lines() {
         let l = result.unwrap();
+        if Regex::new(r"^(#.*|\s*)$").unwrap().is_match(&l) {
+            let _ = writeln!(file2, "{}", l);
+            continue;
+        }
         let caps = Regex::new(r"^([^=]+)=([^:]+):([0-9A-Za-z+/=]+):([^:]*):([0-9A-Za-z+/=]+)$").unwrap().captures(&l).unwrap();
         let y = &base64::decode(caps.get(5).unwrap().as_str()).unwrap();
         let z = crypto::decrypt(caps.get(2).unwrap().as_str(), passwd, caps.get(3).unwrap().as_str(), caps.get(4).unwrap().as_str(), y.to_vec());
