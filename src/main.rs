7da83ac6d36266e4086740146c60c8849a7abc25
jj1uty <jj1uty@jarl.com>
2021-08-16 23:21:42 +0900

    エンコードとデコードの分離

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Write};
 use rand::prelude::*;
 use rand_chacha::ChaCha20Rng;
 use regex::Regex;
@@ -13,9 +13,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let algo = "sha256-aria192-cbc";
     let info = "";
 
+    let mut file = File::create("d")?;
     for result in BufReader::new(File::open("a")?).lines() {
         let l = result?;
-        println!("{}", l);
+        // println!("{}", l);
 
         let mut data = [0u8; 32];
         csp_rng.fill_bytes(&mut data);
@@ -27,13 +28,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let c = base64::encode(&b[..]);
 
         let d = format!("{}={}:{}:{}:{}", r.get(1).unwrap().as_str(), algo, salt, info, c);
-        println!("{}", d);
+        // println!("{}", d);
+        let _ = writeln!(file, "{}", d);
+    }
+    file.flush()?;
 
-        let caps = Regex::new(r"([^=]+)=([^:]+):([0-9A-Za-z+/=]+):([^:]*):([0-9A-Za-z+/=]+)").unwrap().captures(&d).unwrap();
+    let mut file2 = File::create("e")?;
+    for result in BufReader::new(File::open("d")?).lines() {
+        let l = result?;
+        let caps = Regex::new(r"([^=]+)=([^:]+):([0-9A-Za-z+/=]+):([^:]*):([0-9A-Za-z+/=]+)").unwrap().captures(&l).unwrap();
         let y = &base64::decode(caps.get(5).unwrap().as_str()).unwrap();
         let z = crypto::decrypt(caps.get(2).unwrap().as_str(), passwd, caps.get(3).unwrap().as_str(), caps.get(4).unwrap().as_str(), y.to_vec());
-        println!("{}={}", caps.get(1).unwrap().as_str(), z);
-        println!("");
+        let zz = format!("{}={}", caps.get(1).unwrap().as_str(), z);
+        // println!("{}", zz);
+        let _ = writeln!(file2, "{}", zz);
     }
+    file2.flush()?;
     Ok(())
 }
