b8e875e71dd2e26bd6070f6380cf713d597df4c4
jj1uty <jj1uty@jarl.com>
2021-08-16 23:24:44 +0900

    unwrap に変更

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,16 @@ use regex::Regex;
 
 mod crypto;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     let mut csp_rng = ChaCha20Rng::from_entropy();
 
     let passwd = "1234567890";
     let algo = "sha256-aria192-cbc";
     let info = "";
 
-    let mut file = File::create("d")?;
-    for result in BufReader::new(File::open("a")?).lines() {
-        let l = result?;
+    let mut file = File::create("d").unwrap();
+    for result in BufReader::new(File::open("a").unwrap()).lines() {
+        let l = result.unwrap();
 
         let mut data = [0u8; 32];
         csp_rng.fill_bytes(&mut data);
@@ -29,17 +29,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let d = format!("{}={}:{}:{}:{}", r.get(1).unwrap().as_str(), algo, salt, info, c);
         let _ = writeln!(file, "{}", d);
     }
-    file.flush()?;
+    file.flush().unwrap();
 
-    let mut file2 = File::create("e")?;
-    for result in BufReader::new(File::open("d")?).lines() {
-        let l = result?;
+    let mut file2 = File::create("e").unwrap();
+    for result in BufReader::new(File::open("d").unwrap()).lines() {
+        let l = result.unwrap();
         let caps = Regex::new(r"([^=]+)=([^:]+):([0-9A-Za-z+/=]+):([^:]*):([0-9A-Za-z+/=]+)").unwrap().captures(&l).unwrap();
         let y = &base64::decode(caps.get(5).unwrap().as_str()).unwrap();
         let z = crypto::decrypt(caps.get(2).unwrap().as_str(), passwd, caps.get(3).unwrap().as_str(), caps.get(4).unwrap().as_str(), y.to_vec());
         let zz = format!("{}={}", caps.get(1).unwrap().as_str(), z);
         let _ = writeln!(file2, "{}", zz);
     }
-    file2.flush()?;
-    Ok(())
+    file2.flush().unwrap();
 }
