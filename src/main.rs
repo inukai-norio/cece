bd66ab6c6fd70a10c5cf8c51e24222cac341d9cd
jj1uty <jj1uty@jarl.com>
2021-09-14 23:48:35 +0900

    一文字の変数を修正

--- src/main.rs
+++ src/main.rs
@@ -19,22 +19,22 @@ fn encode(infile: &str, outfile: &str, passwd: &str, algo: &str, info: &str) {
     let mut csp_rng = ChaCha20Rng::from_entropy();
     let mut file = File::create(outfile).unwrap();
     for result in BufReader::new(File::open(infile).unwrap()).lines() {
-        let l = result.unwrap();
+        let res = result.unwrap();
 
         let mut data = [0u8; 32];
         csp_rng.fill_bytes(&mut data);
 
         let salt = &base64::encode(&data[..]);
-        if is_comment(&l) {
-            let _ = writeln!(file, "{}", l);
+        if is_comment(&res) {
+            let _ = writeln!(file, "{}", res);
             continue;
         }
-        let r = Regex::new(r"^([^=]+)=(.*)$").unwrap().captures(&l).unwrap();
+        let caps = Regex::new(r"^([^=]+)=(.*)$").unwrap().captures(&res).unwrap();
         
-        let b = crypto::encrypt(algo, passwd, salt, info, r.get(2).unwrap().as_str());
+        let b = crypto::encrypt(algo, passwd, salt, info, caps.get(2).unwrap().as_str());
         let c = base64::encode(&b[..]);
 
-        let d = format!("{}={}:{}:{}:{}", r.get(1).unwrap().as_str(), algo, salt, info, c);
+        let d = format!("{}={}:{}:{}:{}", caps.get(1).unwrap().as_str(), algo, salt, info, c);
         let _ = writeln!(file, "{}", d);
     }
     file.flush().unwrap();
@@ -43,12 +43,12 @@ fn encode(infile: &str, outfile: &str, passwd: &str, algo: &str, info: &str) {
 fn decode(infile: &str, outfile: &str, passwd: &str) {
     let mut file = File::create(outfile).unwrap();
     for result in BufReader::new(File::open(infile).unwrap()).lines() {
-        let l = result.unwrap();
-        if is_comment(&l) {
-            let _ = writeln!(file, "{}", l);
+        let res = result.unwrap();
+        if is_comment(&res) {
+            let _ = writeln!(file, "{}", res);
             continue;
         }
-        let caps = Regex::new(r"^([^=]+)=([^:]+):([0-9A-Za-z+/=]+):([^:]*):([0-9A-Za-z+/=]+)$").unwrap().captures(&l).unwrap();
+        let caps = Regex::new(r"^([^=]+)=([^:]+):([0-9A-Za-z+/=]+):([^:]*):([0-9A-Za-z+/=]+)$").unwrap().captures(&res).unwrap();
         let y = &base64::decode(caps.get(5).unwrap().as_str()).unwrap();
         let z = crypto::decrypt(caps.get(2).unwrap().as_str(), passwd, caps.get(3).unwrap().as_str(), caps.get(4).unwrap().as_str(), y.to_vec());
         let zz = format!("{}={}", caps.get(1).unwrap().as_str(), z);
