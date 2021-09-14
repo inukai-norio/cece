8aa2b207b1f4aabc92f7b8d935ca29b2c59531f7
jj1uty <jj1uty@jarl.com>
2021-09-15 00:11:34 +0900

    map で簡単にした

--- src/crypto.rs
+++ src/crypto.rs
@@ -32,11 +32,7 @@ fn check_algorithm(algorithm: &str)-> Option<[&str; 3]> {
     let re = Regex::new(r"(sha(?:224|256|384|512)|sm3)-((?:aes|aria|camellia)(?:128|192|256)|sm4)-(cbc|cfb|ofb(?:1|8|64|128))").unwrap();
     if re.is_match(algorithm) {
         let caps = re.captures(algorithm).unwrap();
-        let mut ret = [""; 3];
-        for i in 0..3 {
-            ret[i] = caps.get(i+1).unwrap().as_str();
-        }
-        Some(ret)
+        Some([1, 2, 3].map(|i| caps.get(i).unwrap().as_str()))
     } else {
         None
     }
