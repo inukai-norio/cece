930ab638edf9201bb921f16f580bf152a6b8134f
kei-s16 <kei1216s@nifty.com>
2021-11-09 23:46:00 +0900

    master(暫定)にマージされたらリリースタグをくっつけるActionsを作成
    単体では動く
    理想はこれが走ってタグがリポジトリに追加されたらリリースのActionsが走ってくれることだがそこまでは動いてない

---
+++ github/scripts/add-tag.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "{\"ref\":\"refs/tags/release-$(date +"%Y%m%d-%H%m")\",\"sha\":\"$COMMIT\"}" | \
+curl -s -X POST "https://api.github.com/repos/${REPO}/git/refs" \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  -d @-
