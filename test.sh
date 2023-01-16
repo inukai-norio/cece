0238172890906e2c37627bfb0588d4e76598c96f
jj1uty <jj1uty@jarl.com>
2023-01-16 17:44:58 +0900

    テストコマンド修正
    過去のバージョンで作成した暗号化ファイルが復号できないと困るので

--- test.sh
+++ test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+set -eu
+
 cargo run -- -e -i testsfiles/sample.env -o temp > /dev/null 2>&1
 cargo run -- -d -i temp -o temp2 > /dev/null 2>&1
 diff testsfiles/sample.env temp2 > /dev/null
-exit $?
+
+cargo run -- -d -i testsfiles/sample.encrypt.env -o temp2 > /dev/null 2>&1
+diff testsfiles/sample.env temp2 > /dev/null
