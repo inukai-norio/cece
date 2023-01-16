227eed748793cda71f77db0743ea085d4ff5e0a6
jj1uty <jj1uty@jarl.com>
2023-01-16 20:47:49 +0900

    各暗号方式に関してテストするようにする。
    ついでに毎回コンパイルするのではなく、一度だけにする。
    結合テストなので、本番用のバイナリにする。

--- test.sh
+++ test.sh
@@ -2,9 +2,51 @@
 
 set -eu
 
-cargo run -- -e -i testsfiles/sample.env -o temp > /dev/null 2>&1
-cargo run -- -d -i temp -o temp2 > /dev/null 2>&1
-diff testsfiles/sample.env temp2 > /dev/null
+h1=(
+    sha224
+    sha256
+    sha384
+    sha512
+    sm3
+)
 
-cargo run -- -d -i testsfiles/sample.encrypt.env -o temp2 > /dev/null 2>&1
+e1=(
+    aes128
+    aes192
+    aes256
+    aria128
+    aria192
+    aria256
+    camellia128
+    camellia192
+    camellia256
+    sm4
+)
+
+m1=(
+    cbc
+    cfb
+    ofb1
+    ofb8
+    ofb64
+    ofb128
+)
+
+cargo build --release > /dev/null 2>&1
+CECE=./target/release/cece
+
+for h in "${h1[@]}"
+do
+    for e in "${e1[@]}"
+    do
+        for m in "${m1[@]}"
+        do
+            $CECE -e -a "$h-$e-$m" -i testsfiles/sample.env -o temp
+            $CECE -d -i temp -o temp2
+            diff testsfiles/sample.env temp2 > /dev/null
+        done
+    done
+done
+
+$CECE -d -i testsfiles/sample.encrypt.env -o temp2
 diff testsfiles/sample.env temp2 > /dev/null
